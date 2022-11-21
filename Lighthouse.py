3b00474800a783bb5bfea868f1366bc08833e8bf
Lee-HT <58458197+Lee-HT@users.noreply.github.com>
2022-11-21 21:19:28 +0900

    Update Lighthouse.py

--- Lighthouse.py
+++ Lighthouse.py
@@ -1,6 +1,6 @@
 from collections import defaultdict,deque
 
-def light_check(num,lists,light):
+def light_check(num,lists,light): #자신과 연결된 노드의 등대가 켜진지 확인
     if light[num] == True:
         return True
     for l in lists:
@@ -13,11 +13,11 @@ def solution(n, lighthouse):
     path_leaf = deque()
     light = defaultdict(bool)
     
-    for l1,l2 in lighthouse:
+    for l1,l2 in lighthouse: #트리 생성
         path[l1].append(l2)
         path[l2].append(l1)
         
-    for k,v in path.items():
+    for k,v in path.items(): #리프 노드 큐에 입력
         if len(v) == 1:
             path_leaf.append(k)
         
@@ -27,12 +27,12 @@ def solution(n, lighthouse):
             break
         nextn = path[num][0]
         
-        path[nextn].remove(num)
+        path[nextn].remove(num) #확정된 리프 노드를 부모 노드에서 제거
         
         if not light_check(num,path[num],light):
             light[nextn] = True
             
-        if len(path[nextn]) == 1:
+        if len(path[nextn]) == 1: #부모 노드가 리프 노드가 될 시 추가
             path_leaf.append(nextn)
     
     answer = list(light.values()).count(True)
